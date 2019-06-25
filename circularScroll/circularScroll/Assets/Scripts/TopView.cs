299143666e40bbccd97932a3fa5604e88b2bb0f8
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-06-25 16:09:58 +0530

    parsing the sample.json through TopView.cs

---
+++ circularScroll/circularScroll/Assets/Scripts/TopView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TopView : MonoBehaviour {
+    const string sampleJson = "sample.json";
+	
+	void Start () {
+        Debug.Log("start");
+        string fileName = Path.Combine(Application.dataPath, sampleJson);
+        LoadJson(fileName);
+    }
+    public void LoadJson(string fileName)
+    {
+        using (StreamReader r = new StreamReader(fileName))
+        {
+            string json = r.ReadToEnd();
+            Debug.Log("json" + json);
+            ListItem items = JsonUtility.FromJson<ListItem>(json);
+            Debug.Log("***" + items.Values.Length);
+            for(int i=0;i<items.Values.Length;i++)
+            {
+                Debug.Log(items.Values[i].Text);
+            }           
+        }
+    }
+
+}
+[Serializable]
+public class ListItem { 
+    public Values[] Values;
+}
+[Serializable]
+public class Values
+{
+    public string Text;
+}
